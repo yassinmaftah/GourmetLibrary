294fb23be21ffcdca539b371e74cd50e8cf4311f
yassinmftah <yassinmaftah06@gmail.com>
2026-03-11 09:26:44 +0000

    update database seeder with fake library data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,44 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+// use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Category;
+use \App\Models\Book;
+use \App\Models\BookCopy;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Chef Bibliothécaire',
+            'email' => 'admin@gourmet.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
         ]);
+
+        User::factory(5)->create([
+            'role' => 'reader'
+        ]);
+
+        $categories = Category::factory(5)->create();
+
+        foreach ($categories as $category) {
+            $books = Book::factory(10)->create([
+                'category_id' => $category->id
+            ]);
+
+            foreach ($books as $book) {
+                BookCopy::factory(3)->create([
+                    'book_id' => $book->id
+                ]);
+            }
+        }
     }
 }
