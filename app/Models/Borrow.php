69704ce47a21e1fec77eb7fc63b242ea357d0ec3
yassinmftah <yassinmaftah06@gmail.com>
2026-03-11 09:26:32 +0000

    create borrow model and migration

---
+++ app/Models/Borrow.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Borrow extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id', 'book_copy_id', 'borrowed_at',
+        'due_date', 'returned_at', 'status'
+    ];
+
+    protected $casts = [
+        'borrowed_at' => 'datetime',
+        'due_date' => 'datetime',
+        'returned_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bookCopy()
+    {
+        return $this->belongsTo(BookCopy::class);
+    }
+}
